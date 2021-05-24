3f23de981efd1e886b3777a05a511a0bc50aa756
iknight18 <feresnefzi18@gmail.com>
2021-05-24 13:45:52 +0100

    Project init

---
+++ scrumium/src/main/java/tn/enicarthage/scrumium/ScrumiumApplication.java
@@ -0,0 +1,14 @@
+package tn.enicarthage.scrumium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ScrumiumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumiumApplication.class, args);
+	}
+
+}
---
+++ scrumium/src/test/java/tn/enicarthage/scrumium/ScrumiumApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.enicarthage.scrumium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumiumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
